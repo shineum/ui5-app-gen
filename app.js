97b1ed4961bc855cb4624e0b12013d722c3bf6ad
Sungwon <shineum@gmail.com>
2025-01-04 20:59:34 -0800

    update app

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 3000;
+var bodyParser = require("body-parser");
+var UI5AppGen = require("./ui5-app-gen");
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// middlewares
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+// views
+app.get("/",function(req, res){
+    res.render("form");
+});
+app.post("/",function(req, res){
+    // key mappings
+    const tNameMappings = {
+        "app-name": "APP_NAME",
+        "app-id": "APP_ID",
+        "app-title": "APP_TITLE",
+        "app-theme": "APP_THEME",
+        "app-version": "APP_VERSION",
+        "ui5-min-version": "UI5_MIN_VERSION",
+        "ui5-version": "UI5_VERSION",
+    };
+
+    // set values
+    var defaultMappings = UI5AppGen.getAppMappings();
+    Object.entries(tNameMappings).forEach((pEntry) => {
+        if (req.body[pEntry[0]]) {
+            defaultMappings[pEntry[1]] = req.body[pEntry[0]];
+        }
+    });
+
+    // download zip file
+    new UI5AppGen(defaultMappings).sendZipStream(res);
 });
 
+// start server
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
